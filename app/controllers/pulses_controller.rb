f9442da4ce12a49d2ef52431e3807de6ee888e6d
PierreYves Hoorens <pyhoorens@gmail.com>
2022-03-30 15:55:48 +0200

    form new & create pulse WIP

--- app/controllers/pulses_controller.rb
+++ app/controllers/pulses_controller.rb
@@ -4,11 +4,26 @@ class PulsesController < ApplicationController
   end
 
   def new
+    @pulse = Pulse.new
   end
 
   def create
+    @pulse = Pulse.new(pulse_params)
+    @pulse.user = current_user
+
+    if @pulse.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def pulse_params
+    params.require(:pulse).permit(:pulse_content)
   end
 end
--- app/models/pulse.rb
+++ app/models/pulse.rb
@@ -1,3 +1,5 @@
 class Pulse < ApplicationRecord
   belongs_to :user
+  validates :pulse_content, presence: true
+  # mettre une length min et max
 end
