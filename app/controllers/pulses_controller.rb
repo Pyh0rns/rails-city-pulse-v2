919dba96301c512b779ffa478148005fd1179a2f
PierreYves Hoorens <pyhoorens@gmail.com>
2022-04-04 09:31:35 +0200

    categories ok

--- app/controllers/pulses_controller.rb
+++ app/controllers/pulses_controller.rb
@@ -21,6 +21,12 @@ class PulsesController < ApplicationController
     @pulse.user = current_user
 
     if @pulse.save
+      params[:pulse][:category_ids].each do |id|
+        @pulse_categories = PulseCategory.new
+        @pulse_categories.category = Category.find(id)
+        @pulse_categories.pulse = @pulse
+        @pulse_categories.save
+      end
       redirect_to root_path
       flash[:alert] = "C'est pulsé !"
     else
