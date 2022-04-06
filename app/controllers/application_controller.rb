5eca01e9de8c894b1941dcfbf1939bdf78a1f49d
PierreYves Hoorens <pyhoorens@gmail.com>
2022-04-06 13:13:50 +0200

    cloudinary WIP

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :photo])
   end
 end
